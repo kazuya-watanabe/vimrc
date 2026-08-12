7a6da45529f10a6643aa1fe560823b9409aff4f8
渡邉 一也 <kazuya@MacBook-Pro.local>
2026-08-12 19:51:06 +0900

    feat(vim): Fzf接頭辞を廃止し自動生成ファイルを除外設定へ追加:

--- install.ps1
+++
@@ -1,4 +0,0 @@
-$ScriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
-
-(Get-Item -Path "$env:USERPROFILE\vimfiles").Delete()
-New-Item -Force -ItemType Junction -Target "$ScriptPath\.vim" -Path "$env:USERPROFILE\vimfiles"
